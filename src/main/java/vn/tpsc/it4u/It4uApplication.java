0234bce3c147c97a159c9703e21626efea960254
Toan.TNM <toan.tran@tpsc.vn>
2019-11-22 14:50:01 +0700

    Initial commit

---
+++ src/main/java/vn/tpsc/it4u/It4uApplication.java
@@ -0,0 +1,13 @@
+package vn.tpsc.it4u;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class It4uApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(It4uApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/tpsc/it4u/It4uApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.tpsc.it4u;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class It4uApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
