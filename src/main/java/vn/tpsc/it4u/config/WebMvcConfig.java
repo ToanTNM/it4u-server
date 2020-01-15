2d62d05d8d4080c1914853d36d0251099009f2a3
Toan.TNM <toan.tran@tpsc.vn>
2020-01-15 14:01:27 +0700

    Add @Configuration to WebMvcConfig to fix CORS

--- src/main/java/vn/tpsc/it4u/config/WebMvcConfig.java
+++ src/main/java/vn/tpsc/it4u/config/WebMvcConfig.java
@@ -1,11 +1,13 @@
 package vn.tpsc.it4u.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * WebMvcConfig
  */
+@Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
     private final long MAX_AGE_SECS = 3600;
