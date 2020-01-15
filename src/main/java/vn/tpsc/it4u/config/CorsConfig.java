0fc4813549c42a89851fbe00878669774498a645
Toan.TNM <toan.tran@tpsc.vn>
2020-01-15 14:17:41 +0700

    Change WebMvcConfig.java to CorsConfig.java

--- src/main/java/vn/tpsc/it4u/config/CorsConfig.java
+++ src/main/java/vn/tpsc/it4u/config/CorsConfig.java
@@ -2,13 +2,15 @@ package vn.tpsc.it4u.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * WebMvcConfig
  */
 @Configuration
-public class WebMvcConfig implements WebMvcConfigurer {
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
 
     private final long MAX_AGE_SECS = 3600;
 
@@ -16,6 +18,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
+                // .allowedOrigins("http://localhost:8080")
                 .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
                 .maxAge(MAX_AGE_SECS);
     }
