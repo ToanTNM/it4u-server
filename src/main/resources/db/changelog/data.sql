baf14c049835eaa8f8301b97afd327d26791d8ca
Minh Toàn <tnmtoan@yahoo.com>
2020-01-19 21:27:39 +0700

    Insert data to postgres OK

--- src/main/resources/db/changelog/data.sql
+++ src/main/resources/db/changelog/data.sql
@@ -1,10 +1,8 @@
 INSERT INTO roles(name) VALUES('ROLE_USER');
 INSERT INTO roles(name) VALUES('ROLE_ADMIN');
-
+GO
 INSERT INTO USERS(EMAIL, GENDER, NAME, PASSWORD, STATUS, TYPE, USERNAME, SITENAME) VALUES('email@yahoo.com', 'Male', 'admin', '$2a$10$fnEjJgyjJzrYlEmfDuufxOM.Evjmhocmu.2JaQ3R8r31sTUHujFgS', 'Active', 'User', 'admin', 'rhnl7chn');
-
-INSERT INTO USER_ROLES VALUES('1','2');
-
 INSERT INTO USERS(EMAIL, GENDER, NAME, PASSWORD, STATUS, TYPE, USERNAME, SITENAME) VALUES('test@yahoo.com', 'Male', '49lqd', '$2a$10$fnEjJgyjJzrYlEmfDuufxOM.Evjmhocmu.2JaQ3R8r31sTUHujFgS', 'Active', 'User', 'KSHT', 'rhnl7chn');
-
+GO
+INSERT INTO USER_ROLES VALUES('1','2');
 INSERT INTO USER_ROLES VALUES('2','1');
