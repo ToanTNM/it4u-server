77709b2d45044a8f9dde4c0e80c9b5f5c592f6b6
Lap Dang <lap.dang@tpsc.vn>
2020-12-24 16:54:19 +0700

    Fixed delete user

--- src/main/java/vn/tpsc/it4u/model/User.java
+++ src/main/java/vn/tpsc/it4u/model/User.java
@@ -97,7 +97,7 @@ public class User extends UserDateAudit {
     private String resetToken;
 
     //@DBRef(lazy = true)
-    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @ManyToMany
     @JoinTable(name = "user_roles",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
