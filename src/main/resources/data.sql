81f511f6a27f18229f2059e7db776cd04ba2596d
Minh Toàn <tnmtoan@yahoo.com>
2022-06-02 09:48:52 +0700

    Update

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,41 +1,41 @@
 -- #######Postgres
-INSERT INTO roles
-  (NAME)
-VALUES
-  ('ROLE_USER'),
-  ('ROLE_ADMIN')
-ON CONFLICT
-(NAME) DO NOTHING;
-
-INSERT INTO USERS
-  (EMAIL, GENDER, NAME, PASSWORD, STATUS, TYPE, USERNAME, SITENAME)
-VALUES
-  ('email@yahoo.com', 'Male', 'admin', '$2a$10$fnEjJgyjJzrYlEmfDuufxOM.Evjmhocmu.2JaQ3R8r31sTUHujFgS', 'Active', 'User', 'admin', 'rhnl7chn'),
-  ('test@yahoo.com', 'Male', '49lqd', '$2a$10$fnEjJgyjJzrYlEmfDuufxOM.Evjmhocmu.2JaQ3R8r31sTUHujFgS', 'Active', 'User', 'test', 'rhnl7chn')
-ON CONFLICT
-(EMAIL) DO NOTHING;
-
-INSERT INTO USER_ROLES
-VALUES
-  ('1', '2'),
-  ('2', '1')
-ON CONFLICT
-(USER_ID, ROLE_ID) DO NOTHING;
-
--- ########## OTHER DB
 -- INSERT INTO roles
---   (name)
--- VALUES('ROLE_USER');
--- INSERT INTO roles
---   (name)
--- VALUES('ROLE_ADMIN');
+--   (NAME)
+-- VALUES
+--   ('ROLE_USER'),
+--   ('ROLE_ADMIN')
+-- ON CONFLICT
+-- (NAME) DO NOTHING;
 
 -- INSERT INTO USERS
 --   (EMAIL, GENDER, NAME, PASSWORD, STATUS, TYPE, USERNAME, SITENAME)
--- VALUES('email@yahoo.com', 'Male', 'admin', '$2a$10$fnEjJgyjJzrYlEmfDuufxOM.Evjmhocmu.2JaQ3R8r31sTUHujFgS', 'Active', 'User', 'admin', 'rhnl7chn');
+-- VALUES
+--   ('email@yahoo.com', 'Male', 'admin', '$2a$10$fnEjJgyjJzrYlEmfDuufxOM.Evjmhocmu.2JaQ3R8r31sTUHujFgS', 'Active', 'User', 'admin', 'rhnl7chn'),
+--   ('test@yahoo.com', 'Male', '49lqd', '$2a$10$fnEjJgyjJzrYlEmfDuufxOM.Evjmhocmu.2JaQ3R8r31sTUHujFgS', 'Active', 'User', 'test', 'rhnl7chn')
+-- ON CONFLICT
+-- (EMAIL) DO NOTHING;
 
 -- INSERT INTO USER_ROLES
--- VALUES('1', '2');
+-- VALUES
+--   ('1', '2'),
+--   ('2', '1')
+-- ON CONFLICT
+-- (USER_ID, ROLE_ID) DO NOTHING;
+
+-- ########## OTHER DB
+INSERT INTO roles
+  (name)
+VALUES('ROLE_USER');
+INSERT INTO roles
+  (name)
+VALUES('ROLE_ADMIN');
+
+INSERT INTO USERS
+  (EMAIL, GENDER, NAME, PASSWORD, STATUS, TYPE, USERNAME, SITENAME)
+VALUES('email@yahoo.com', 'Male', 'admin', '$2a$10$fnEjJgyjJzrYlEmfDuufxOM.Evjmhocmu.2JaQ3R8r31sTUHujFgS', 'Active', 'User', 'admin', 'rhnl7chn');
+
+INSERT INTO USER_ROLES
+VALUES('1', '2');
 
 -- INSERT INTO USERS
 --   (EMAIL, GENDER, NAME, PASSWORD, STATUS, TYPE, USERNAME, SITENAME)
