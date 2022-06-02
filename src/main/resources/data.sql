083413533420251ce9fa07132504bb79147e6745
Minh Toàn <tnmtoan@yahoo.com>
2022-06-02 09:30:00 +0700

    Update: - Parameterized jwtSecret - Update data.sql to ignore if conflict

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,45 @@
-INSERT INTO roles(name) VALUES('ROLE_USER');
-INSERT INTO roles(name) VALUES('ROLE_ADMIN');
+-- #######Postgres
+INSERT INTO roles
+  (NAME)
+VALUES
+  ('ROLE_USER'),
+  ('ROLE_ADMIN')
+ON CONFLICT
+(NAME) DO NOTHING;
 
-INSERT INTO USERS(EMAIL, GENDER, NAME, PASSWORD, STATUS, TYPE, USERNAME, SITENAME) VALUES('email@yahoo.com', 'Male', 'admin', '$2a$10$fnEjJgyjJzrYlEmfDuufxOM.Evjmhocmu.2JaQ3R8r31sTUHujFgS', 'Active', 'User', 'admin', 'rhnl7chn');
+INSERT INTO USERS
+  (EMAIL, GENDER, NAME, PASSWORD, STATUS, TYPE, USERNAME, SITENAME)
+VALUES
+  ('email@yahoo.com', 'Male', 'admin', '$2a$10$fnEjJgyjJzrYlEmfDuufxOM.Evjmhocmu.2JaQ3R8r31sTUHujFgS', 'Active', 'User', 'admin', 'rhnl7chn'),
+  ('test@yahoo.com', 'Male', '49lqd', '$2a$10$fnEjJgyjJzrYlEmfDuufxOM.Evjmhocmu.2JaQ3R8r31sTUHujFgS', 'Active', 'User', 'test', 'rhnl7chn')
+ON CONFLICT
+(EMAIL) DO NOTHING;
 
-INSERT INTO USER_ROLES VALUES('1','2');
+INSERT INTO USER_ROLES
+VALUES
+  ('1', '2'),
+  ('2', '1')
+ON CONFLICT
+(USER_ID, ROLE_ID) DO NOTHING;
 
-INSERT INTO USERS(EMAIL, GENDER, NAME, PASSWORD, STATUS, TYPE, USERNAME, SITENAME) VALUES('test@yahoo.com', 'Male', '49lqd', '$2a$10$fnEjJgyjJzrYlEmfDuufxOM.Evjmhocmu.2JaQ3R8r31sTUHujFgS', 'Active', 'User', 'test', 'rhnl7chn');
+-- ########## OTHER DB
+-- INSERT INTO roles
+--   (name)
+-- VALUES('ROLE_USER');
+-- INSERT INTO roles
+--   (name)
+-- VALUES('ROLE_ADMIN');
 
-INSERT INTO USER_ROLES VALUES('2','1');
+-- INSERT INTO USERS
+--   (EMAIL, GENDER, NAME, PASSWORD, STATUS, TYPE, USERNAME, SITENAME)
+-- VALUES('email@yahoo.com', 'Male', 'admin', '$2a$10$fnEjJgyjJzrYlEmfDuufxOM.Evjmhocmu.2JaQ3R8r31sTUHujFgS', 'Active', 'User', 'admin', 'rhnl7chn');
+
+-- INSERT INTO USER_ROLES
+-- VALUES('1', '2');
+
+-- INSERT INTO USERS
+--   (EMAIL, GENDER, NAME, PASSWORD, STATUS, TYPE, USERNAME, SITENAME)
+-- VALUES('test@yahoo.com', 'Male', '49lqd', '$2a$10$fnEjJgyjJzrYlEmfDuufxOM.Evjmhocmu.2JaQ3R8r31sTUHujFgS', 'Active', 'User', 'test', 'rhnl7chn');
+
+-- INSERT INTO USER_ROLES
+-- VALUES('2', '1');
